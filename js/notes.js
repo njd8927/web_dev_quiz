5c37fee61b23f0170209079899607e52d3e8bc30
Nicholas Zentai <nicholasz8687@gmail.com>
2023-03-19 12:55:05 -0400

    updated html & js syntax

--- js/notes.js
+++ js/notes.js
@@ -6,23 +6,17 @@ var quest = document.querySelector('#quest');
 var timerDisplay = document.querySelector('#timer-display');
 var answerDisplay = document.querySelector('#answer-display');
 var nextBtn = document.querySelector('#next-button');
-var choice1 = document.querySelector('#choice-1-btn');
-var choice2 = document.querySelector('#choice-2-btn');
-var choice3 = document.querySelector('#choice-3-btn');
-var choice4 = document.querySelector('#choice-4-btn');
 
-                                                                                  
+var currentQuestion = questions;
 
-var currentQuestionIndex = 0;                                                        //CREATE A VARIABLE TO TRACK CURRENT QUESTION INDEX                                       
-
-var currentQuestion; 
-var timer;                                                             //CREATE A VARIABLE THAT WILL HOLD THE CURRENT QUESTION OBJECT (QUESTIONS & ANSWERS ARE THE OBJECTS)                                                    
-
-
-var count = 10;                                                                     //CREATE A VARIABLE TO STORE THE CURRENT TIMER COUNT
+//CREATE A VARIABLE TO TRACK CURRENT QUESTION INDEX
+var currentQuestionIndex = 0;
+//CREATE A VARIABLE THAT WILL HOLD THE CURRENT QUESTION OBJECT (QUESTIONS & ANSWERS ARE THE OBJECTS)
+var currentQuestion;
+//CREATE A VARIABLE TO STORE THE CURRENT TIMER COUNT
+var count = 10;
 
 //FUNCTION THAT IS CALLED WHEN THE END OF THE QUESTIONS ARRAY IS REACHED THAT PROMPTS USER TO RESTART
-
 function promptUserToRestart() {
     var userChoice = confirm('Would you like to restart?');
 
@@ -30,23 +24,19 @@ function promptUserToRestart() {
         displayQuestion();
     } else {
         startBtn.classList.remove('hide');
-        questionDisplay.textContent = 'Have a great day!';
+        questionDisplay.innerText = 'Have a great day!';
         answerDisplay.classList.add('hide');
         nextBtn.classList.add('hide');
     }
 }
-
 //FUNCTION TO HIDE THE TIMER AND SHOW THE ANSWER AND NEXT BUTTON
-
 function showAnswer() {
-    answerDisplay.textContent = currentQuestion.choices[currentQuestion.correctIndex];
+    answerDisplay.innerText = question.answer;
     timerDisplay.classList.add('hide');
     nextBtn.classList.remove('hide');
     answerDisplay.classList.remove('hide');
-    
-    
 
-    
+    currentQuestionIndex++;
 
     // IF THE CURRENTQUESTIONINDEX IS EQUAL TO THE QUESTIONS ARRAY LENGTH, 
     // THEN WE STOP QUESTIONS AND CONFIRM IF THE USER WOULD LIKE TO RESTART
@@ -58,79 +48,58 @@ function showAnswer() {
     }
 }
 
-// CREATE A FUNCTION THAT STARTS A TIMER AT 10 SECONDS 
+// CREATE A FUNCTION THAT STARTS A TIMER AT 5 SECONDS 
 // AND COUNTS DOWN TO ZERO, THEN CALLS SHOWANSWER
-
 function startTimer() {
     timerDisplay.classList.remove('hide');
-    timerDisplay.textContent = 'Count: ' + count;
-    
+    timerDisplay.innerText = '10';
     // CREATE A SETINTERVAL AND STORE IT TO A VARIABLE THAT TRIGGERS EVERY SECOND
-
-    timer = setInterval(function () {
+    var timer = setInterval(function () {
         // DECREASE COUNT BY ONE
         count--;
         // SET TIMER DISPLAY TO THE COUNT VARIABLE
-        timerDisplay.textContent = 'Count: ' + count;
+        timerDisplay.innerText = count;
         // IF COUNT IS EQUAL TO ZERO, STOP TIMER AND SHOW ANSWER/NEXT BUTTON
-        if (count <= 0) {
-            // !COUNT = NOT QUAL
+        if (!count) {
+            // !COUNT = NOT COUNT
             clearInterval(timer);
-            count = 10;
+            count = 5;
             showAnswer();
         }
     }, 1000);
 }
-
 // CREATE A FUNCTION THAT GRABS THE CURRENT CARD OBJECT 
 // AND DISPLAYS THE QUESTION TO THE WINDOW
-
 function displayQuestion() {
-    clearInterval(timer);
     // CREATE A VARIABLE REFRENCE TO THE CURRENT QUESTION OBJECT FROM THE QUESTIONS ARRAY
-    currentQuestion = questions[currentQuestionIndex];
+    var question = questions[currentQuestionIndex];
 
     questionDisplay.classList.remove('hide');
-    console.log(currentQuestion);
-    quest.innerText = currentQuestion.text
-    choice1.textContent = currentQuestion.choices[0];
-    choice2.textContent = currentQuestion.choices[1];
-    choice3.textContent = currentQuestion.choices[2];
-    choice4.textContent = currentQuestion.choices[3];
 
-
-
-
+    questionDisplay.innerText = question.question;
     // QUESTION DISPLAY IS NOW SET TO THE FIRST QUESTION (QUESTION.QUESTION)
     // QUESTION IS THE NEW VARIABLE WHICH HAS A VALUE OF THE CURRENT QUESTION INDEX OF 
     // THE QUESTIONS VARIABLE WITH THE QUESTIONS & ANSWERS
 
     answerDisplay.classList.add('hide');
-    nextBtn.classList.remove('hide');
-    currentQuestionIndex++;
+    nextBtn.classList.add('hide');
     startTimer();
-    
 }
 
 
 
-// HIDE THE START BUTTON AND CALL THE DISPLAY QUESTION FUNCTION ABOVE
-
+// HIDE THE START BUTTON AND CALL THE DISPLAY CARD FUNCTION ABOVE
 function startQuestions() {
     startBtn.classList.add('hide');
     displayQuestion();
+    startTimer();
 }
 
 // WHEN USER CLICKS START BUTTON, SHOW THE FIRST QUESTION
-
 startBtn.addEventListener('click', startQuestions);
 nextBtn.addEventListener('click', displayQuestion);
-choice1.addEventListener('click', showAnswer);
-choice2.addEventListener('click', showAnswer);
-choice3.addEventListener('click', showAnswer);
-choice4.addEventListener('click', showAnswer);
-
-
+// WHEN TIMER RUNS OUT, SHOW THE ANSWER
+// SHOW A NEXT BUTTON TO ALLOW THE USER TO MOVE ONTO NEXT QUESTION
 
 
 // WHEN THE END OF THE QUESTIONS ARRAY HAS BEEN REACHED, SHOW THE USER A CONFIRMATION
--- js/question_answer.js
+++ js/question_answer.js
@@ -1,63 +1,112 @@
 var questions = [
     {
         text: 'In the terminal, what command is used to create a new file?',
-        choices: ['touch', 'mkdir', 'gitcommit', 'gitcheckout'],
+        choices: [
+            'touch',
+            'mkdir',
+            'gitcommit',
+            'gitcheckout'
+        ],
         correctIndex: 0
     },
 
     {
-
         text: 'What is the correct syntax to call a function?',
-        choices: ['func:', 'func;', 'func()', 'func='],
+        choices: [
+            'func:', 
+            'func;', 
+            'func()', 
+            'func='
+        ],
         correctIndex: 2
     },
 
     {
         text: 'Which value in the following list does NOT represent a primitive value?',
-        choices: ['undefined', 'str', 'NaN', 'boolean'],
+        choices: [
+            'undefined', 
+            'str', 
+            'NaN', 
+            'boolean'
+        ],
         correctIndex: 2
     },
-    
+
     {
 
         text: 'What tag is used to define an image to an HTML page?',
-        choices: ['table', 'img', 'div', 'image'],
+        choices: [
+            'table', 
+            'img',
+            'div', 
+            'image'
+        ],
         correctIndex: 1
     },
 
     {
         text: 'What tag is used to transform text into italics?',
-        choices: ['i', 'strong', 'em', 'it'],
+        choices: [
+            'i', 
+            'strong', 
+            'em', 
+            'it'
+        ],
         correctIndex: 1,
     },
 
     {
         text: 'What tag is used to define a numbered list?',
-        choices: ['ul', 'li', 'nl', 'ol'],
+        choices: [
+            'ul', 
+            'li', 
+            'nl', 
+            'ol'
+        ],
         correctIndex: 3,
     },
 
     {
         text: 'What declaration must be included as the first item in an HTML document?',
-        choices: ['!DOCTYPE', 'embed', 'html', 'code'],
+        choices: [
+            '!DOCTYPE', 
+            'embed', 
+            'html', 
+            'code'
+        ],
         correctIndex: 0,
     },
 
     {
         text: 'To target a class in CSS, what character is used before the class name?',
-        choices: ['#', '*', '&', '.'],
+        choices: [
+            '#', 
+            '*', 
+            '&', 
+            '.'
+        ],
         correctIndex: 3,
     },
 
     {
         text: 'To target an id in CSS, what character is used before the class name?',
-        choices: ['.', '&', '#', '*'],
+        choices: [
+            '.', 
+            '&', 
+            '#', 
+            '*'
+        ],
         correctIndex: 2,
     },
 
     {
         text: 'What is it called when you refer to a variable name by the purpose or type of the variable?',
-        choices: ['Hungarian Notation', 'camelCase', 'snakecase', 'coding'],
+        choices: [
+            'Hungarian Notation', 
+            'camelCase', 
+            'snakecase', 
+            'coding'
+        ],
         correctIndex: 0,
     },
 ];
